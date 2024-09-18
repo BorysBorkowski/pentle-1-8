f6b08aebca3d85de744453c25bdbe13a0b673abf
BorysBorkowski <borysbork2009@gmail.com>
2024-09-18 15:40:43 +0200

    Add files via upload

---
+++ pentle6/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=40; i>=31; i--)
+    {
+       cout << i << endl;
+    }
+    return 0;
+}
---
+++ pentle7/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   for(int i=49; i>=41; i--)
+   {
+       cout << i << endl;
+   }
+    return 0;
+}
